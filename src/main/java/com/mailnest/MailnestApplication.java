0fc3966a08b81176c803c3d5c5b77d4fce7c6f44
imam-007 <imimam8409@gmail.com>
2025-04-10 00:17:27 +0530

    project setup

---
+++ src/main/java/com/mailnest/MailnestApplication.java
@@ -0,0 +1,13 @@
+package com.mailnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailnestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mailnest/MailnestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
