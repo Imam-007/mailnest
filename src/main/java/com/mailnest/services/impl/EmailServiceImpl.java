a943d7a4b53e5d4231a16d01ead973ea0bbcd8b9
imam-007 <imimam8409@gmail.com>
2025-04-10 22:38:54 +0530

    added code for implement all method in emil service

--- src/main/java/com/mailnest/services/impl/EmailServiceImpl.java
+++ src/main/java/com/mailnest/services/impl/EmailServiceImpl.java
@@ -1,11 +1,16 @@
 package com.mailnest.services.impl;
 
 import com.mailnest.services.EmailService;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -33,15 +38,53 @@ public class EmailServiceImpl implements EmailService {
     @Override
     public void sendEmail(String[] to, String subject, String message) {
 
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(message);
+        simpleMailMessage.setFrom("maimam8409@gmail.com");
+        javaMailSender.send(simpleMailMessage);
+        log.info("send email to multiple person");
+
     }
 
     @Override
     public void sendEmailWithHtml(String to, String subject, String htmlContent) {
 
+        MimeMessage simpleMailMessage = javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(simpleMailMessage, true, "UTF-8");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setFrom("maimam8409@gmail.com");
+            helper.setText(htmlContent);
+            javaMailSender.send(simpleMailMessage);
+            log.info("send html content");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public void sendEmailWithFile(String to, String subject, String message, File file) {
+
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setFrom("imimam8409@gmail.com");
+            helper.setTo(to);
+            helper.setText(message);
+            helper.setSubject(subject);
+            FileSystemResource fileSystemResource = new FileSystemResource(file);
+            helper.addAttachment(fileSystemResource.getFilename(), file);
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
