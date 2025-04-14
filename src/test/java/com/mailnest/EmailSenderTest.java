b86d24a3fbc6118096e1fcc0ed7673551311e246
imam-007 <imimam8409@gmail.com>
2025-04-14 23:08:10 +0530

    added test file code

--- src/test/java/com/mailnest/EmailSenderTest.java
+++ src/test/java/com/mailnest/EmailSenderTest.java
@@ -1,9 +1,12 @@
 package com.mailnest;
 
+import com.mailnest.dto.Message;
 import com.mailnest.services.EmailService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class EmailSenderTest {
@@ -16,5 +19,33 @@ public class EmailSenderTest {
     void emailSend(){
         System.out.println("send email");
         emailService.sendEmail("imimam8409@gmail.com", "email from imam", "het how are you");
+    }
+
+    @Test
+    void emailSends(){
+        System.out.println("send email to multiple person");
+        String[] email = {"imimam8409@gmail.com", "mdaurangzebimam15299@gmail.com"};
+        emailService.sendEmail(email, "email from imam", "emil to mulitiple person");
+    }
+
+    @Test
+    void sendHtml(){
+        String html = "" +
+                "<h1 style='color:red;'> Hii i am imam </h1>" +
+                "";
+
+        emailService.sendEmailWithHtml("imimam8409@gmail.com", "form imam", html);
+    }
+
+    @Test
+    void getInbox() {
+        List<Message> inboxMessage = emailService.getInboxMessage();
+
+        inboxMessage.forEach(item -> {
+            System.out.println(item.getSubject());
+            System.out.println(item.getContent());
+            System.out.println(item.getFiles());
+            System.out.println("=====================================");
+        });
     }
 }
